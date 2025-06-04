d48d88c6c32a94788eb7901f6c0e565328883922
Thắm Phan Thị Hồng <thampth@kaopiz.com>
2025-06-05 00:27:01 +0700

    update information + socket

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const { Server } = require("socket.io");
 
 const dbLoad = require("./db/dbLoad");
 const dbConnect = require("./db/dbConnect");
@@ -10,6 +10,7 @@ const UserRouter = require("./routes/UserRouter");
 const PhotoRouter = require("./routes/PhotoRouter");
 const CommentRouter = require("./routes/CommentRouter");
 const { verifyToken } = require("./middleware/auth");
+const { createServer } = require("http");
 
 const port = 8080;
 
@@ -23,7 +24,39 @@ app.use("/api/user", UserRouter);
 app.use("/api/photosOfUser", PhotoRouter);
 app.use("/api/commentsOfUser", CommentRouter);
 app.use("/uploads", express.static("uploads"));
+
 app.listen(port, () => {
   console.log(`Sandbox listening on port ${port}`);
 });
 
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }
+})
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("sendComment", (data) => {
+    console.log("User comment");
+    io.emit("newComment", data);
+  })
+
+  socket.on("uploadPhoto", (data) => {
+    console.log("User upload photo");
+    io.emit("newPhoto", data);
+  })
+  
+  socket.on("updateUser", (data) => {
+    console.log("User update information");
+    io.emit("newUser", data);
+  })
+})
+
+server.listen(3001, () => {
+  console.log(`Server is running on port 3001`);
+});
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -25,4 +25,32 @@ router.get("/:userId", verifyToken, async (req, res) => {
   }
 });
 
+router.put("/:userId", verifyToken, async (req, res) => {
+  const {
+    first_name,
+    last_name,
+    location,
+    description,
+    occupation
+  } = req.body;
+
+  if (!first_name || !last_name) {
+    return res.status(400).json({
+      success: false,
+      message: "Please enter all fields"
+    });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true });
+
+    if (user) res.status(200).json({ success: true, data: user });
+    else res.status(404).json({ success: false, msg: "User not found" });
+    
+  } catch (err) {
+    console.log("❌ Error get user:", err);
+    res.status(500).json({ success: false, msg: err });
+  }
+});
+
 module.exports = router;
